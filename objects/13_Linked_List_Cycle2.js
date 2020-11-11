c925fa41c304b0f632beac30d2f3a5696bcec06c
Andrei <Kulik>
2020-11-11 12:16:38 +0300

    feat: add tasks

--- objects/13_Linked_List_Cycle2.js
+++ objects/13_Linked_List_Cycle2.js
@@ -1,4 +1,20 @@
 //https://leetcode.com/problems/linked-list-cycle-ii/
 
-
+let detectCycle = function (head) {
+    let map = {}
+    while (head) {
+        if (map[head.val]) {
+            for (let i = 0; i < map[head.val].length; i++) {
+                if (head === map[head.val][i]) {
+                    return map[head.val][i]
+                }
+            }
+            map[head.val].push(head)
+        } else {
+            map[head.val] = [head]
+        }
+        head = head.next
+    }
+    return null
+}
 
